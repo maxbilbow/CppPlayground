746fc8a6bc7214a0d5b891e453e564f481c85322
Max Bilbow <max.bilbow@utilisoft.com>
2017-12-22 13:56:40 +0000

    vectors

--- lesson13_vectors.cpp
+++ lesson13_vectors.cpp
@@ -117,13 +117,208 @@ namespace lesson13_vectors {
 
     }
 
+    void emplace()
+    {
+//creating a vector of integers
+      std::vector<int> vectorInts;
+      //creating an iterator for the vector
+      std::vector<int>::iterator it;
+
+      vectorInts.push_back(0);
+      vectorInts.push_back(1);
+      vectorInts.push_back(2);
+      vectorInts.push_back(3);
+      vectorInts.push_back(4);
+      vectorInts.push_back(5);
+      vectorInts.push_back(6);
+      vectorInts.push_back(7);
+
+      std::cout<<"vectorInts has "<<vectorInts.size()<<" elements\n";
+
+      //printing the contents of vectorInts
+      std::cout<<"VectorInts has these elements:\n";
+      for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
+        std::cout<<*it<<" ";
+
+      //insert an element after the first element
+      it  = vectorInts.begin() + 1;
+      vectorInts.emplace(it, -1);
+      std::cout<<"\n\nAfter the emplace(it, -1)\n";
+      for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
+        std::cout<<*it<<" ";
+
+      //insert an element after the third element
+      it  = vectorInts.begin();
+      vectorInts.emplace(it + 3, -2);
+      std::cout<<"\n\nAfter the emplace(it + 3, -2)\n";
+      for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
+        std::cout<<*it<<" ";
+
+      //insert an element after the third element
+      it  = vectorInts.begin();
+      vectorInts.emplace(it + 5, -3);
+      std::cout<<"\n\nAfter the emplace(it + 5, -3)\n";
+      for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
+        std::cout<<*it<<" ";
+
+
+    }
+
+    void emplace2()
+    {
+      struct Foo
+      {
+          int n, x;
+          Foo(int nIn, double xIn)
+          {
+            n = nIn; x = static_cast<int>(xIn);
+          }
+      };
+
+      std::vector<Foo> v;
+      v.emplace(v.begin()+1, 42, 3);
+      v.insert(v.begin()+1, Foo(42, 3));
+    }
+    using namespace std;
+    void printVector(vector<int> vIn);
+    void assignFunction(vector<int> vInts, int in);
+    void pushBackFunction(vector<int> vInts, int in);
+    void emplaceFunction(vector<int> vInts, int loc, int in);
+
+    void clear()
+    {
+      //creating a vector of integers
+      std::vector<int> vectorInts;
+      //creating an iterator for the vector
+      std::vector<int>::iterator it;
+
+      vectorInts.push_back(0);
+      vectorInts.push_back(1);
+      vectorInts.push_back(2);
+      vectorInts.push_back(3);
+      vectorInts.push_back(4);
+      vectorInts.push_back(5);
+      vectorInts.push_back(6);
+      vectorInts.push_back(7);
+
+      std::cout<<"vectorInts has "<<vectorInts.size()<<" elements\n";
+
+      //printing the contents of vectorInts
+      std::cout<<"VectorInts has these elements:\n";
+      for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
+        std::cout<<*it<<" ";
+
+      //clear the vector
+      vectorInts.clear();
+      std::cout<<"\nAfter clear, the vector has these elements:\n";
+      for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
+        std::cout<<*it<<" ";
+
+    }
+
+    void erase()
+    {
+      //creating a vector of integers
+      std::vector<int> vectorInts;
+      //creating an iterator for the vector
+      std::vector<int>::iterator it;
+
+      vectorInts.push_back(0);
+      vectorInts.push_back(1);
+      vectorInts.push_back(2);
+      vectorInts.push_back(3);
+      vectorInts.push_back(4);
+      vectorInts.push_back(5);
+      vectorInts.push_back(6);
+      vectorInts.push_back(7);
+
+      std::cout<<"vectorInts has "<<vectorInts.size()<<" elements\n";
+
+      //printing the contents of vectorInts
+      std::cout<<"VectorInts has these elements:\n";
+      for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
+        std::cout<<*it<<" ";
+
+      //clear the vector
+      vectorInts.erase(vectorInts.begin()+4);
+      std::cout<<"\nAfter erase, the vector has these elements:\n";
+      for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
+        std::cout<<*it<<" ";
+    }
+
+    void popBack()
+    {
+      //creating a vector of integers
+      std::vector<int> vectorInts;
+      //creating an iterator for the vector
+      std::vector<int>::iterator it;
+
+      vectorInts.push_back(0);
+      vectorInts.push_back(1);
+      vectorInts.push_back(2);
+      vectorInts.push_back(3);
+      vectorInts.push_back(4);
+      vectorInts.push_back(5);
+      vectorInts.push_back(6);
+      vectorInts.push_back(7);
+
+      std::cout<<"vectorInts has "<<vectorInts.size()<<" elements\n";
+
+      //printing the contents of vectorInts
+      std::cout<<"VectorInts has these elements:\n";
+      for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
+        std::cout<<*it<<" ";
+
+      //pop the last element off the vector
+      vectorInts.pop_back();
+      std::cout<<"\n\nAfter pop_back(), the vector has these elements:\n";
+      for (it = vectorInts.begin(); it != vectorInts.end(); ++it)
+        std::cout<<*it<<" ";
+
+      //pop_back does not return the element that was removed.
+      //For example, the line of code below will return an error.
+      int a;
+      //a = vectorInts.pop_back(); //this line does not compile
+    }
     int main ()
     {
       changeSize();
       assign();
       pushBack();
       insert();
+      emplace();
+      clear();
+      erase();
+      popBack();
     }
 
 
+    void printVector(vector<int> vIn)
+    {//printing the contents of vIns
+      vector<int>::iterator it;
+
+      for (it = vIn.begin(); it != vIn.end(); ++it)
+        std::cout<<*it<<" ";
+    }
+
+    void assignFunction(vector<int> vInts, int in)
+    {
+      cout<<"\nassigning "<<in<<" and printing the vector\n";
+      vInts.assign(1, in);
+      printVector(vInts);
+    }
+    void pushBackFunction(vector<int> vInts, int in)
+    {
+      cout<<"\npush back "<<in<<" and printing the vector\n";
+      vInts.push_back(in);
+      printVector(vInts);
+    }
+    void emplaceFunction(vector<int> vInts,  int loc, int in)
+    {
+      vector<int>::iterator it;
+      cout<<"\nemplacing "<<in<<" and printing the vector\n";
+      it  = vInts.begin() + loc;
+      vInts.emplace(it, in);
+      printVector(vInts);
+    }
 }
